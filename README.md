fd611fe466d83a981cfe36ae8043b02917e2b215
shha256 <alisson.abilio56@gmail.com>
2024-09-07 07:25:05 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # cluster_tf
+=======
+# devops-tf
+>>>>>>> c89c551 (Initial commit)
---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  tags = {
+    Department   = "Devops"
+    Organization = "Infrastructure and Operations"
+    Project      = "Eks"
+    Enviroment   = "Development"
+  }
+
+}
