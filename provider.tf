0e38a91137e6b711ae1d0a61aa0af61f5bd41815
shha256 <alisson.abilio56@gmail.com>
2024-09-13 14:02:28 -0300

    update no providers name

--- provider.tf
+++ provider.tf
@@ -25,13 +25,13 @@ provider "aws" {
   shared_credentials_files = ["~/.aws/credentials"]
 }
 
-provider "kubernets" {
+provider "kubernetes" {
   host                   = module.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
   exec {
 
 
-    api_version = "clinet.autentication.k8s.io/v1"
+    api_version = "client.authentication.k8s.io/v1"
     args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
     command     = "aws"
   }
